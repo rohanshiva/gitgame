133bb3b2224841dde4e7fcd1c9eedf93e779f101
Ramko9999 <ramapitchala@gmail.com>
2022-01-04 15:17:03 -0600

    Moved around test files/folders

--- server/test/routes/ws_test.py
+++ server/test/routes/ws_test.py
@@ -1,8 +1,5 @@
 from fastapi.testclient import TestClient
-from fastapi import HTTPException
 from main import app
-
-from server.gitgame.routes import session
 
 client = TestClient(app)
 
@@ -18,7 +15,6 @@ def test_make_session_invalid_authors():
         response.json()["detail"]
         == "Failed to make a session due to invalid usernames: ['ramko99', 'ramko989']"
     )
-
 
 def test_join_session_invalid_session_id():
     session_id = "wrong_session_id"
@@ -42,27 +38,3 @@ def test_join_session_invalid_username():
     with client.websocket_connect(socket_url) as websocket:
         data = websocket.receive_json()
         assert data == {"error": f"invalid username: {invalid_username}"}
-
-
-def test_join_session():
-    response = client.post(
-        "/session/make",
-        headers={"accept": "application/json", "Content-Type": "application/json"},
-        json=["rohanshiva", "ramko9999"],
-    )
-    assert response.status_code == 201
-    session_id = response.json()["id"]
-    username = "TanushN"
-    socket_url = f"/socket/{session_id}/{username}"
-    with client.websocket_connect(socket_url) as websocket:
-        data = websocket.receive_json()
-        assert data == {"msg": f"{username} connected lobby {session_id}"}
-    response = client.get(
-        f"/session/{session_id}", headers={"accept": "application/json"}
-    )
-    assert response.status_code == 200
-    response_data = response.json()
-    response_authors = response_data["authors"]
-    response_players = response_data["players"]
-    assert response_players == [username]
-    assert response_authors == ["rohanshiva", "ramko9999", username]
