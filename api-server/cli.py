d2413d6c51ee057843de4465da6d9080463e1940
Ramko9999 <ramapitchala@gmail.com>
2022-11-16 21:27:38 -0600

    add delete_rows cmd to CLI

--- api-server/cli.py
+++ api-server/cli.py
@@ -4,39 +4,55 @@ import anyio
 import asyncpg
 from config import DB_URI
 
-# A simple CLI for different management operations for the API
-def wipe_db_tables(args: argparse.Namespace):
-    async def drop_all(db_uri: str):
+
+def drop_tables(args: argparse.Namespace):
+    async def drop_all_tables(db_uri: str):
         db_conn = await asyncpg.connect(db_uri)
         tables = await db_conn.fetch(
             "SELECT * FROM pg_tables WHERE schemaname = 'public'"
         )
-        if tables:
-            drops = []
-            for table in tables:
-                drops.append(f"DROP TABLE IF EXISTS {table['tablename']} CASCADE;")
-            drop_statement = "\n".join(drops)
-            print("Drop query:\n", drop_statement)
-            await db_conn.execute(drop_statement)
-        else:
-            print("No tables to drop...")
+        drops = []
+        for table in tables:
+            drops.append(f"DROP TABLE IF EXISTS {table['tablename']} CASCADE;")
+        drop_statement = "\n".join(drops)
+        await db_conn.execute(drop_statement)
 
     resolved_db_uri = DB_URI.replace("host.docker.internal", "localhost")
-    anyio.run(drop_all, resolved_db_uri)
-    print("Successfully dropped tables!")
+    anyio.run(drop_all_tables, resolved_db_uri)
+    print("Successfully dropped all tables!")
+
+
+def delete_rows(args: argparse.Namespace):
+    async def delete_all_rows(db_uri: str):
+        db_conn = await asyncpg.connect(db_uri)
+        tables = await db_conn.fetch(
+            "SELECT * FROM pg_tables WHERE schemaname = 'public'"
+        )
+        deletes = []
+        for table in tables:
+            deletes.append(f"DELETE FROM {table['tablename']} CASCADE;")
+        delete_statement = "\n".join(deletes)
+        await db_conn.execute(delete_statement)
+
+    resolved_db_uri = DB_URI.replace("host.docker.internal", "localhost")
+    anyio.run(delete_all_rows, resolved_db_uri)
+    print("Successfully deleted all rows!")
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="An utility to help manually manage the DB and the other tools/services when developing the API",
+        description="A CLI to manage the DB and other tools when developing the Gitgame API",
     )
 
     subparsers = parser.add_subparsers(
         title="Sub-commands", help="The list of sub-commands to possibly run"
     )
     subparsers.add_parser(
-        "wipe_tables", help="Wipes all the database tables and rows"
-    ).set_defaults(func=wipe_db_tables)
+        "drop_tables", help="Drops all the database tables"
+    ).set_defaults(func=drop_tables)
+    subparsers.add_parser(
+        "delete_rows", help="Deletes all database table rows"
+    ).set_defaults(func=delete_rows)
 
     args = parser.parse_args(sys.argv[1:])
     args.func(args)
