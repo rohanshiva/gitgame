bc9d26e24967730b39a2b7b3b1e42ca208a5ee65
Ramko9999 <ramapitchala@gmail.com>
2022-12-15 17:05:51 -0600

    add loading of repos and files

--- api-server/routes/session.py
+++ api-server/routes/session.py
@@ -1,10 +1,12 @@
 import logging
 import random
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Depends, status
 from nanoid import generate
 from pydantic import BaseModel
 from tortoise.transactions import in_transaction
 from models import Session, Player
+from services.github_client import GithubClient, GithubUserNotFound
+from config import GITHUB_ACCESS_TOKEN
 
 router = APIRouter(prefix="/session", tags=["Session"])
 logger = logging.getLogger()
@@ -50,19 +52,34 @@ async def get_lobby(session: Session) -> Lobby:
     return Lobby(players=lobby_players)
 
 
-@router.post(
-    "/join/{session_id}", status_code=status.HTTP_202_ACCEPTED, response_model=Lobby
-)
-async def join(session_id: str, join_body: JoinRequestBody):
-    # todo(ramko9999): select for update may prevent insertions of rows which reference session_id via Fkey. Consider passing in no_key
-    # or using an other table just for locking lobby related activities such as joining, leaving or scoring the game.
-    session = await Session.filter(id=session_id).select_for_update().first()
+async def verify_session(session_id: str):
+    session = await Session.get_or_none(id=session_id)
     if not session:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND, f"Session {session_id} not found"
         )
+
+
+def get_gh_client():
+    return GithubClient(GITHUB_ACCESS_TOKEN)
+
+
+@router.post(
+    "/join/{session_id}",
+    status_code=status.HTTP_202_ACCEPTED,
+    response_model=Lobby,
+    dependencies=[Depends(verify_session)],
+)
+async def join(
+    session_id: str,
+    join_body: JoinRequestBody,
+    gh_client: GithubClient = Depends(get_gh_client),
+):
+    # todo(ramko9999): select for update may prevent insertions of rows which reference session_id via Fkey. Consider passing in no_key
+    # or using an other table just for locking lobby related activities such as joining, leaving or scoring the game.
     player_id = f"{session_id}-{join_body.username}"
     async with in_transaction():
+        session = await Session.filter(id=session_id).select_for_update().first()
         player = await Player.get_or_none(id=player_id)
         if player and player.connection_state == Player.ConnectionState.CONNECTED:
             raise HTTPException(
@@ -81,6 +98,15 @@ async def join(session_id: str, join_body: JoinRequestBody):
                 connection_state=Player.ConnectionState.CONNECTED,
             )
             await player.save()
+            try:
+                await player.load_repos(gh_client)
+            except GithubUserNotFound:
+                raise HTTPException(
+                    status.HTTP_404_NOT_FOUND,
+                    f"Player's username {join_body.username} is not a valid Github username",
+                )
+            await player.load_files(gh_client)
+
         if session.host is None:
             session.host = player.id
             await session.save(update_fields=["host"])
@@ -92,17 +118,15 @@ class LeaveRequestBody(BaseModel):
 
 
 @router.post(
-    "/leave/{session_id}", status_code=status.HTTP_202_ACCEPTED, response_model=Lobby
+    "/leave/{session_id}",
+    status_code=status.HTTP_202_ACCEPTED,
+    response_model=Lobby,
+    dependencies=[Depends(verify_session)],
 )
 async def leave(session_id: str, leave_body: LeaveRequestBody):
-    session = await Session.filter(id=session_id).select_for_update().first()
-    if not session:
-        # todo(Ramko9999): make this session check a dependency
-        raise HTTPException(
-            status.HTTP_404_NOT_FOUND, f"Session {session_id} not found"
-        )
     player_id = f"{session_id}-{leave_body.username}"
     async with in_transaction():
+        session = await Session.filter(id=session_id).select_for_update().first()
         player = await Player.get_or_none(id=player_id)
         if not player or player.connection_state == Player.ConnectionState.DISCONNECTED:
             raise HTTPException(
