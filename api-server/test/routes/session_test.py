3535162451fd50ffb1aacddc67d695c16f36b11a
rohan <rhnshiva@gmail.com>
2023-02-20 14:27:54 -0800

    add test for creating context w/o context

--- api-server/test/routes/session_test.py
+++ api-server/test/routes/session_test.py
@@ -7,7 +7,7 @@ from models import Session
 @pytest.mark.usefixtures("clear_db")
 class TestMakeSession:
     @pytest.mark.anyio
-    async def test_make_session(self, api_client: AsyncClient):
+    async def test_make_session_with_context(self, api_client: AsyncClient):
         cookies = Cookies()
         username = "test_username"
         cookies.set("token", Auth.encode(username))
@@ -19,3 +19,8 @@ class TestMakeSession:
         session = await Session.filter(id=response_json["id"]).first()
         assert session is not None
         assert session.host is None
+    
+    @pytest.mark.anyio
+    async def test_make_session_without_context(self, api_client: AsyncClient):
+        response = await api_client.post("/session/make")
+        assert response.status_code == 401
