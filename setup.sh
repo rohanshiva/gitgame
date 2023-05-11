4266b4002cb457b9c257d700c4ad3007921a29a1
Ramko9999 <ramapitchala@gmail.com>
2023-05-10 23:36:27 -0500

    add nginx setup

--- setup.sh
+++ setup.sh
@@ -9,22 +9,33 @@ sudo apt install libpq-dev gcc python3-dev -y
 # install pip
 curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.10
 sudo python3.10 -m pip install pip --upgrade
+echo "Setup Python!"
 
 # checkout production
-git clone https://github.com/rohanshiva/gitgame.git
-cd gitgame/
 git fetch origin production
 git checkout production
 
 # setup API
 cd api/
 sudo python3.10 -m pip install -r requirements.txt
+echo "Setup API!"
 
 # setup nodejs
 sudo apt install nodejs npm xsel -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.nvm/nvm.sh
 nvm install lts/hydrogen
 nvm use lts/hydrogen
 
 # for serving build/
 npm install serve -g
+echo "Setup Node!"
+
+# install nginx
+sudo apt install nginx -y
+sudo unlink /etc/nginx/sites-enabled/default
+sudo cp nginx.conf /etc/nginx/conf.d
+sudo nginx -s reload
+echo "Setup Nginx!"
+
+cp ../api.env ./api/.env
