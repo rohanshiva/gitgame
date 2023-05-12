9db4c4ceff5a2ea53ff8c2d427efde40cbb01500
Ramko9999 <ramapitchala@gmail.com>
2023-05-12 00:49:13 -0500

    run api and web app in the background

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/bash
 run_api(){
     cd ./api;
-    python3.10 main.py
+    nohup python3.10 main.py&
 }
 
 run_web(){
     cd ./client;
-    npx serve build > website.log
+    nohup npx serve build > website.log&
 }
 
-source ~/.nvm/nvm.sh
+kill_existing_runs(){
+    # kills any existing python or npx server processes
+    kill -9 $(ps aux | grep -i -e "python3.10 main.py" -e "npm exec serve build" | grep -v "grep" | awk '{print $2}')
+}
+
+kill_existing_runs
 run_api & run_web
