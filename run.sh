13e06a0fa88d0f27b0c7ee5e4daa2be01a675f0d
Ramko9999 <ramapitchala@gmail.com>
2023-05-15 15:50:55 -0500

    enable https

--- run.sh
+++ run.sh
@@ -14,5 +14,11 @@ kill_existing_runs(){
     kill -9 $(ps aux | grep -i -e "python3.10 main.py" -e "npm exec serve -s build" | grep -v "grep" | awk '{print $2}')
 }
 
+run_nginx(){
+    sudo cp nginx.conf /etc/nginx/conf.d
+    sudo nginx -s reload
+}
+
 kill_existing_runs
+run_nginx
 run_api & run_web
