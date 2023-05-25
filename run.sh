45a641be3ab17ea62feb4f7fd5369bb781d9abfa
Ramko9999 <ramapitchala@gmail.com>
2023-05-25 01:36:47 -0500

    add prometheus and grafana run steps

--- run.sh
+++ run.sh
@@ -9,9 +9,21 @@ run_web(){
     nohup npx serve -s build > website.log&
 }
 
+run_prometheus(){
+    cp prometheus.yml ../prometheus-*/prometheus.yml;
+    cd ../prometheus-*;
+    nohup ./prometheus --config.file=prometheus.yml&
+}
+
+run_grafana(){
+    nohup ../grafana-*/bin/grafana server --homepath ../grafana-*&
+}
+
 kill_existing_runs(){
     # kills any existing python or npx server processes
-    kill -9 $(ps aux | grep -i -e "python3.10 main.py" -e "npm exec serve -s build" | grep -v "grep" | awk '{print $2}')
+    kill -9 $(ps aux | grep -i -e "python3.10 main.py" -e "serve -s build" | grep -v "grep" | awk '{print $2}')
+    # kill prometheus and grafana
+    kill -9 $(ps aux | grep -i -e "prometheus" -e "grafana" | grep -v "grep" | awk '{print $2}')
 }
 
 run_nginx(){
@@ -21,4 +33,4 @@ run_nginx(){
 
 kill_existing_runs
 run_nginx
-run_api & run_web
+run_api & run_web & run_prometheus & run_grafana
