f9e6273424fa8b66eb383c3eb78b85d807c256eb
Ramko9999 <ramapitchala@gmail.com>
2023-06-24 14:42:18 -0400

    redirect non-matching routes to /

--- client/src/routers/Router.tsx
+++ client/src/routers/Router.tsx
@@ -1,8 +1,12 @@
 import { useContext } from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "../components/home";
 import Game from "../components/game";
-import CommonRoute from "./CommonRoute";
 import UserContext from "../context/UserContext";
 import { baseRoutes_ } from "../constants/Route";
 
@@ -10,16 +14,21 @@ function AppRouter() {
   const { user } = useContext(UserContext);
 
   if (!user) {
-    return (
-      <Home />
-    )
+    return <Home />;
   }
 
   return (
     <Router>
       <Switch>
-        <CommonRoute component={Game} path={baseRoutes_.game} />
-        <CommonRoute component={Home} path={baseRoutes_.root} />
+        <Route path={baseRoutes_.game} exact={true}>
+          <Game/>
+        </Route>
+        <Route path={baseRoutes_.root} exact={true}>
+            <Home/>
+        </Route>
+        <Route path="*">
+          <Redirect to={baseRoutes_.root}/>
+        </Route>
       </Switch>
     </Router>
   );
