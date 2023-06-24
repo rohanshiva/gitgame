a6cdab86e733dcb7d47714474158b3a3deb3c55d
rohan <rhnshiva@gmail.com>
2023-06-24 15:41:15 -0700

    add help vids to dialog

--- client/src/components/commentSider/help/Help.tsx
+++ client/src/components/commentSider/help/Help.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import * as Icon from "react-feather";
 import "./Help.css";
+import config from "../../../config";
 
 interface HelpCommentProps {
   children: ReactNode;
@@ -21,11 +22,16 @@ function Help() {
     <HelpComment>
       <strong>
         <code className="title">
-        How to create a comment?
-          </code></strong>
+          How to create a comment?
+        </code></strong>
       <ul className="help-list">
         <li>
           Click on any line number.
+          <div className="help-video-container">
+            <video muted controls>
+              <source src={config.helpLineClickUri} type="video/mp4" />
+            </video>
+          </div>
         </li>
         <li>
           To select multiple lines, hold{" "}
@@ -33,19 +39,39 @@ function Help() {
             <kbd>shift</kbd>
           </strong>{" "}
           and click on any line number below or above.
+          <div className="help-video-container">
+            <video muted controls>
+              <source src={config.helpShiftLineClickUri} type="video/mp4" />
+            </video>
+          </div>
         </li>
         <li>
           Right click within the highlighted region to trigger
           the comment creation popup.
+          <div className="help-video-container">
+            <video muted controls>
+              <source src={config.helpTriggerCommentMenuUri} type="video/mp4" />
+            </video>
+          </div>
         </li>
         <li>
           Type your comment in the popover texarea.
         </li>
         <li>
           If you don't like something, submit a 💩 comment.
+          <div className="help-video-container">
+            <video muted controls>
+              <source src={config.helpAddPoopCommentUri} type="video/mp4" />
+            </video>
+          </div>
         </li>
         <li>
           If you like something, submit a 💎 comment.
+          <div className="help-video-container">
+            <video muted controls>
+              <source src={config.helpAddDiamondCommentUri} type="video/mp4" />
+            </video>
+          </div>
         </li>
         <li>
           Cancel line selection by clicking on ❌.
