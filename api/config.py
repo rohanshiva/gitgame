b0d4aef4964f7f7027410c270d0d530787b83292
Ramko9999 <ramapitchala@gmail.com>
2023-07-23 16:53:20 -0400

    make disable_auth an environment variable

--- api/config.py
+++ api/config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import argparse
 
 load_dotenv()
 
@@ -11,16 +10,4 @@ GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET")
 AUTH_SECRET = os.environ.get("AUTH_SECRET")
 CLIENT_URL = os.environ.get("CLIENT_URL")
-
-"""
-Added this bit here to make local testing easy. Run `python main.py -d`
-
-Example: `python main.py -d` to run app without auth
-"""
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-d", "--disable-auth", action="store_true", required=False)
-
-args = parser.parse_args()
-
-DISABLE_AUTH = args.disable_auth
+DISABLE_AUTH = os.environ.get("DISABLE_AUTH")
